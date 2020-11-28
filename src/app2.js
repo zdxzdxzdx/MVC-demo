11838ec3d50c112ceed8ae7263af6028178aa875
zdxzdx1219 <769371987@qq.com>
2020-11-28 09:53:45 +0800

    first commit

---
+++ src/app2.js
@@ -0,0 +1,19 @@
+import "./app2.css";
+import $ from "jquery";
+
+const $tabBar = $("#app2 .tab-bar");
+const $tabContent = $("#app2 .tab-content");
+
+$tabBar.on("click", "li", (e) => {
+  const $li = $(e.currentTarget);
+  const index = $li.index();
+  $li.addClass("selected").siblings().removeClass("selected");
+
+  $tabContent
+    .children()
+    .eq(index)
+    .addClass("active")
+    .siblings()
+    .removeClass("active");
+});
+$tabBar.children().eq(0).trigger("click");
